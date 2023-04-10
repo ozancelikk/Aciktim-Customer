6cf4ef189deab93165dfce192beb393332490655
Yusuf Göktaş <yusufgk125@gmail.com>
2023-04-10 12:52:09 +0300

    Müşteri Ana sayfa
    müşteri ana sayfası başlangıç

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { FeaturedRestaurantsComponent } from './components/featured-restaurants/featured-restaurants.component';
+import { RestaurantsComponent } from './components/restaurants/restaurants.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatRadioModule} from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    SliderComponent,
+    CategoriesComponent,
+    FeaturedRestaurantsComponent,
+    RestaurantsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/featured-restaurants/featured-restaurants.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-restaurants',
+  templateUrl: './featured-restaurants.component.html',
+  styleUrls: ['./featured-restaurants.component.css']
+})
+export class FeaturedRestaurantsComponent {
+
+}
