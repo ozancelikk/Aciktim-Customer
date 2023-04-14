1c8ceb26ea7d6af8f30dbd1bef0e98965f459178
Yusuf Göktaş <yusufgk125@gmail.com>
2023-04-14 13:53:52 +0300

    navbar
    navbar update ve resim yolu değiştirildi

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  constructor(){}
+  ngOnInit(): void {}
 
+  isAuthenticated() {
+    if(localStorage.getItem('token')) {
+      return true;
+    }
+    return false;
+  }
 }
