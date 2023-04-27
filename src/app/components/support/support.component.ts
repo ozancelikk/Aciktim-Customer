7b03801e86e7cf1df754100bd59ea820d456cf10
Yusuf Göktaş <yusufgk125@gmail.com>
2023-04-27 12:39:07 +0300

    support
    support kısmı eklendi

--- src/app/components/support/support.component.ts
+++ src/app/components/support/support.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { SupportService } from 'src/app/services/support/support.service';
 
 @Component({
   selector: 'app-support',
   templateUrl: './support.component.html',
   styleUrls: ['./support.component.css']
 })
-export class SupportComponent {
+export class SupportComponent implements OnInit {
+  customerId: string
+  supportForm: FormGroup
+  constructor(private toastrService: ToastrService, private supportService: SupportService, private formBuilder: FormBuilder) { }
+  ngOnInit(): void {
+    this.getCustomerId();
+    this.createSupportForm();
+  }
+
+  createSupportForm() {
+    this.supportForm = this.formBuilder.group({
+      customerId: [this.customerId, Validators.required],
+      mail: ["", Validators.required],
+      content: ["", Validators.required],
+      subject: ["", Validators.required]
+    })
+  }
+
+  getCustomerId() {
+    this.customerId = localStorage.getItem('customerId');
+  }
+
+  addSupportMessage() {
+    //ilk önce giriş yapsın
+    if (!localStorage.getItem('customerId')) {
+      this.toastrService.error("Lütfen ilk önce giriş yapınız", "HATA");
+    }
+    else {
+      if (this.supportForm.valid) {
+        let model = Object.assign({},this.supportForm.value);
+        this.supportService.addSupportMessage(model).subscribe(response=>{
+          if(response.success) {
+            this.toastrService.success("Mesajınız başarıyla gönderildi!","BAŞARILI");
+          }
+        })
+        
+      }
+      else {
+        this.toastrService.error("Lütfen gerekli yerleri doldurunuz.","HATA");
+      }
+
+    }
+
+  }
 
 }
+
---
+++ src/app/models/support/support.ts
@@ -0,0 +1,6 @@
+export interface Support {
+    customerId:string;
+    mail:string;
+    subject:string;
+    content:string;
+}
